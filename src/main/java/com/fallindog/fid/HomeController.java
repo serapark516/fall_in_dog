d82e1cd8877fd3817d0adc34d18ea574b49ed126
namgu0202 <skarndwlsdud801@gmail.com>
2022-11-07 01:07:17 +0900

    메인페이지

--- src/main/java/com/fallindog/fid/HomeController.java
+++ src/main/java/com/fallindog/fid/HomeController.java
@@ -10,11 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -22,8 +24,8 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+	public String index(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -35,5 +37,24 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@RequestMapping(value="/aboutUs")
+	public ModelAndView aboutUs(ModelAndView mv) {
+		mv.setViewName("/about-us");
+		
+		return mv;
+	} //aboutUs
+	
+	@RequestMapping(value="/singlePost")
+	public ModelAndView singlePost(ModelAndView mv) {
+		mv.setViewName("/single-post");
+		return mv;
+	} //singlePost
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact(ModelAndView mv) {
+		mv.setViewName("/contact");
+		return mv;
+	} //contact
 	
 }
